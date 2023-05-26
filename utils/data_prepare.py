4171ab41a18ed123a78cc7b52a4abd0267425208
madara-tribe <48679574+madara-tribe@users.noreply.github.com>
2023-05-26 12:53:19 +0900

    Update data_prepare.py

--- utils/data_prepare.py
+++ utils/data_prepare.py
@@ -1,6 +1,5 @@
 import cv2
 import os
-import json
 import glob
 import numpy as np
 from tqdm import tqdm
@@ -9,7 +8,7 @@ from utils import img_padding
 
 root="datasets/dataset"
 nppath="datasets/npy"
-
+ 
 os.makedirs(nppath, exist_ok=True)
 
 def preprocess(p, size=224):
@@ -18,8 +17,7 @@ def preprocess(p, size=224):
     img = img_padding(img, desired_size=size)
     return img
 
-def ToNumpy(X, y, test_size=0.1):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=33)
+def ToNumpy(X_train, y_train, X_test, y_test):
     print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
     np.save(os.path.join(nppath, "X_train"), X_train)
     np.save(os.path.join(nppath, "y_train"), y_train)
@@ -27,7 +25,7 @@ def ToNumpy(X, y, test_size=0.1):
     np.save(os.path.join(nppath, "y_test"), y_test)
 
 def prepare_data(cfg):
-    imgs, labels = [], []
+    X_train, X_test, y_train, y_test, stack = [], [], [], [], []
     label_names = os.listdir(root)
     label_names = sorted(label_names)
     for cls, label in enumerate(tqdm(label_names)):
@@ -35,11 +33,24 @@ def prepare_data(cfg):
         print(cls, label, path)
         for p in glob.glob(path):
             img = preprocess(p, size=int(cfg.input_size))
-            imgs.append(img)
-            labels.append(int(cls))
+            if cls not in stack:
+                print('test 1', cls, label, path)
+                stack.append(cls)
+                X_test.append(img)
+                y_test.append(cls)
+            elif stack.count(cls)<10:
+                print('test 2', cls, label, path)
+                stack.append(cls)
+                X_test.append(img)
+                y_test.append(cls)
+            else:
+                print('train', cls, label, path)
+                stack.append(cls)
+                X_train.append(img)
+                y_train.append(cls)
     
-    X = np.array(imgs)
-    y = np.array(labels)
-    ToNumpy(X, y, test_size=0.05)
-
-
+    X_train = np.array(X_train)
+    y_train = np.array(y_train)
+    X_test = np.array(X_test)
+    y_test = np.array(y_test)
+    ToNumpy(X_train, y_train, X_test, y_test)
